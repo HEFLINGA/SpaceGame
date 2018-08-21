b64e97cfaca5c8e2a99c583a137f85409baade38
Auston Hefling <auston.hefling@outlook.com>
2018-08-21 09:05:47 -0400

    Added some TODOs to remember some addition needed for ships and story. Implemented ship selection

--- SpaceGame/Program.cs
+++ SpaceGame/Program.cs
@@ -12,16 +12,46 @@ namespace SpaceGame
         {
             // Variable decleration
             string character;
+            string ship1 = "1";
+            string ship2 = "2";
+            string ship3 = "3";
+            string input;
 
-            // Intro line and story
-            Console.WriteLine("Welcome to SpaceGame!!");
-            Console.WriteLine("Story");
 
             // Character selection
             Console.WriteLine("Enter your name: ");
             character = Console.ReadLine();
 
-            Console.WriteLine("Welcome {0}!!", character);
+            Console.WriteLine("Welcome {0} to, THE SPACE GAAMMMEEEEE", character);
+
+            // Intro story
+            // TODO - Add character name to story where names are said ----
+            Console.WriteLine("Story");
+            Console.ReadLine();
+
+            // Buy your ship
+            // TODO - Add ships description and price
+            Console.WriteLine("Buy your first ship! Use the 1, 2 and 3 key to select your option: ");
+
+            input = Console.ReadLine();
+
+            if (input == ship1)
+            {
+                Console.WriteLine("Ship 1 bought");
+            }
+            else if (input == ship2)
+            {
+                Console.WriteLine("Ship 2 bought");
+            }
+            else if (input == ship3)
+            {
+                Console.WriteLine("Ship 3 bought");
+            }
+            else
+            {
+                Console.WriteLine("No Valid input");
+            }
+
         }
     }
 }

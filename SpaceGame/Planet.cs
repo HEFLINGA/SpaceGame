99919bd91e324253e1c825dcd75a047617af0d22
marcus lucious <marcus.j.lucious@outlook.com>
2018-08-22 16:15:04 -0400

    working on location

--- SpaceGame/Planet.cs
+++ SpaceGame/Planet.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpaceGame
 {
-    class Planet
+    public class Planet
     {
 
         public string Type { get; set; }
         public double Date { get; set; }
         public double Rate { get; set; }
-        public double Location { get; set; }
-
+        public double LocationX { get; set; }
+        public double LocationY { get; set; }
+        //public double cordX { get; set; }
+        //public double cordY { get; set; }
     }        
 }

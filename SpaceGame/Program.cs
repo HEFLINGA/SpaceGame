4f9f1f043651e83203868daf89cd082fce8123c0
Auston Hefling <auston.hefling@outlook.com>
2018-08-22 08:46:48 -0400

    Time

--- SpaceGame/Program.cs
+++ SpaceGame/Program.cs
@@ -8,6 +8,34 @@ namespace SpaceGame
 {
     class Program
     {
+        public static bool GameOver(double money, double time)
+        {
+            bool gameOver = false;
+            if ((money >= 0) || (time >= 40))
+            {
+                gameOver = true;
+            }
+            else
+            {
+                gameOver = false;
+            }
+
+            return gameOver;
+        }
+
+        public static double TimePassage(double distance)
+        {
+            double totalTime = 40;
+            double time = 0;
+            bool timeOut = false;
+
+            if (time >= totalTime)
+            {
+                
+            }
+            
+        }
+
         static void Main(string[] args)
         {
             // string decleration
@@ -24,79 +52,102 @@ namespace SpaceGame
 
             // double decleration
             double speed = 0;
+            double time = 0;
 
             // Ship instantiation
-            Ship tier1 = new Ship();
-            tier1.Name = "RustBucket";
-            tier1.Speed = 3;
-            tier1.Price = 5000;
-            tier1.Cargo = 2;
+            Ship tier1 = new Ship
+            {
+                Name = "RustBucket",
+                Speed = 3,
+                Price = 5000,
+                Cargo = 2
+            };
 
-            Ship tier2 = new Ship();
-            tier2.Name = "Speedyy";
-            tier2.Speed = 4;
-            tier2.Price = 15000;
-            tier2.Cargo = 4;
+            Ship tier2 = new Ship
+            {
+                Name = "Speedyy",
+                Speed = 4,
+                Price = 15000,
+                Cargo = 4
+            };
 
-            Ship tier3 = new Ship();
-            tier3.Name = "USS Schwifty Ship";
-            tier3.Speed = 6;
-            tier3.Price = 30000;
-            tier3.Cargo = 6;
+            Ship tier3 = new Ship
+            {
+                Name = "USS Schwifty Ship",
+                Speed = 6,
+                Price = 30000,
+                Cargo = 6
+            };
 
             // Cargo instantiation            
 
-            Cargo food = new Cargo();
-            food.Type = "Food";
-            food.Size = 1;
-            food.Cost = 2000;
-            food.Speed = -.5;
+            Cargo food = new Cargo
+            {
+                Type = "Food",
+                Size = 1,
+                Cost = 2000,
+                Speed = -.5
+            };
 
-            Cargo research = new Cargo();
-            research.Type = "Research";
-            research.Size = 1;
-            research.Cost = 3000;
-            research.Speed = -.5;
+            Cargo research = new Cargo
+            {
+                Type = "Research",
+                Size = 1,
+                Cost = 3000,
+                Speed = -.5
+            };
 
-            Cargo animal = new Cargo();
-            animal.Type = "Animal";
-            animal.Size = 1;
-            animal.Cost = 4000;
-            animal.Speed = -.5;
+            Cargo animal = new Cargo
+            {
+                Type = "Animal",
+                Size = 1,
+                Cost = 4000,
+                Speed = -.5
+            };
 
-            Cargo water = new Cargo();
-            water.Type = "Water";
-            water.Size = 2;
-            water.Cost = 5000;
-            water.Speed = -1;
+            Cargo water = new Cargo
+            {
+                Type = "Water",
+                Size = 2,
+                Cost = 5000,
+                Speed = -1
+            };
 
-            Cargo fuel = new Cargo();
-            fuel.Type = "Fuel";
-            fuel.Size = 2;
-            fuel.Cost = 6000;
-            fuel.Speed = -1;
+            Cargo fuel = new Cargo
+            {
+                Type = "Fuel",
+                Size = 2,
+                Cost = 6000,
+                Speed = -1
+            };
 
             // Planet Instantiation
 
-            Planet myPlanet = new Planet();
-            myPlanet.Type = "EARTH";
-            myPlanet.Date = System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
-            myPlanet.Cost = 1.00;
+            Planet myPlanet = new Planet
+            {
+                Type = "EARTH",
+                Date = System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"),
+                Cost = 1.00
+            };
 
-            Planet myPlanet2 = new Planet();
-            myPlanet2.Type = "TRAPPIST-1";
-            myPlanet2.Date = System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss" + 6);
-            myPlanet2.Cost = (1.00 - 0.86) / 0.86 * 100.00;
-
-
-            Planet myPlanet3 = new Planet();
-            myPlanet3.Type = "ALPHA CENTAURI";
-            myPlanet3.Date = System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss" + 8.5);
-            myPlanet3.Cost = (1.00 - 3.79) / 3.709 * 100.00;
+            Planet myPlanet2 = new Planet
+            {
+                Type = "TRAPPIST-1",
+                Date = System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss" + 6),
+                Cost = (1.00 - 0.86) / 0.86 * 100.00
+            };
 
 
+            Planet myPlanet3 = new Planet
+            {
+                Type = "ALPHA CENTAURI",
+                Date = System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss" + 8.5),
+                Cost = (1.00 - 3.79) / 3.709 * 100.00
+            };
 
-                // Intro line and story
+
+
+            // Intro line and story
             Console.WriteLine("Welcome to SpaceGame!!");
             Console.WriteLine();
             Console.WriteLine("  The year is 3018.A guy a guy or girl of your choice relative passed and left them 10000 dollars. Their family use to be rich merchants but " +
@@ -172,7 +223,7 @@ namespace SpaceGame
 
             // Player starts his journey exploring and buying
 
-                while (credits != 0)
+                while (GameOver(credits, time))
                 {
                 // Console
                 Console.SetCursorPosition(0, 0);

7803858a799154e4528516ab462b0b2f774794eb
marcus lucious <marcus.j.lucious@outlook.com>
2018-08-29 10:30:31 -0400

    move variables for v.cs to new class planet.cs

--- SpaceGame/Planet.cs
+++ SpaceGame/Planet.cs
@@ -8,5 +8,26 @@ namespace SpaceGame
 {
     class Planet
     {
+        public double x = 0;
+        public double y = 0;
+        public string planetName = "";
+        public int currentPlanet = 0;
+
+
+        public Planet()
+        {
+
+        }
+
+        public Planet(string planetName, double x, double y, int currentPlanet)
+        {
+            this.planetName = planetName;
+            this.x = x;
+            this.y = y;
+            this.currentPlanet = currentPlanet;
+
+
+        }
+
     }
 }
